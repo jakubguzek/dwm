584d6884752f3a7c8df6d3d8a8697f2cbb0d4da5
Jakub Guzek <jakub.j.guzek@gmail.com>
2019-11-07 21:14:15 +0100

    bottom bar because screen is fucked

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappoh    = 15;       /* horiz outer gap between windo
 static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#1a1a1a";
